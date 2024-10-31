3422a0b1810358a3701152b7f1f40763288b1d58
tezsaw <thkearl@gmail.com>
2024-10-30 17:26:39 -0700

    Added book model, updated views

---
+++ BigFootLibrary/BigFootLibrary/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace BigFootLibrary.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public bool IsCheckedOut { get; set; }
+        public int CheckOutPersonId { get; set; }
+        public DateTime DueForReturn { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public string CoverImgUrl { get; set; }
+    }
+}
